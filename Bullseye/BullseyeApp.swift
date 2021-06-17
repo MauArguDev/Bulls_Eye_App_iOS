d104b1b4c659109ca37ad0df2428b5f7e230ad68
Mauricio Hernandez <mauricio.hernandez@devsu.com>
2021-06-17 15:35:06 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by user199344 on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
