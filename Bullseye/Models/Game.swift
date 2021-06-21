d5df167dd6964c5105ba738ff3946173d9ae623a
Mauricio Hernandez <mauricio.hernandez@devsu.com>
2021-06-21 09:55:19 -0500

    first commit

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Game{
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var current = 1
+    
+    func points(sliderValue: Int) -> Int {
+        
+        let awardedPoint = 100 - (abs(target - sliderValue))
+        
+        return awardedPoint
+    }
+}
+    
--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -9,52 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private  var alertIsVisible: Bool = false
-    @State private var sliderValue: Double = 50.0
+    @State private  var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
     
     var body: some View {
-        VStack {
+        ZStack{
+            Color("BackgroundColor")
             VStack {
-                VStack {
-                    Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                        .bold()
-                        .kerning(2.0)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(4.0)
-                        .font(.footnote)
-                    Text("89")
-                        .kerning(-1.0)
-                        .font(.largeTitle)
-                        .fontWeight(.black)
+                Text("🎯🎯🎯\nput the bullseye as close as you can to".uppercased())
+                    .bold()
+                    .kerning(2.0)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                Text(String(game.target))
+                    .kerning(-1.0)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
                     
-                    HStack {
-                        Text("1")
-                            .font(.body)
-                            .bold()
-                        Slider(value: self.$sliderValue, in:
-                                1.0...100.0)
-                        Text("100")
-                            .font(.body)
-                            .bold()
-                    }.padding(10)
-                    VStack {
-                        Button(action: {
-                            self.alertIsVisible = true
-                        }) {
-                            Text("HIT ME")
-                        }
-                        .alert(isPresented: $alertIsVisible,
-                            content: {
-                                let roundedValue: Int = Int(self.sliderValue.rounded())
-                                return Alert(title: Text("Hi there!"), message: Text("The sliders values is \(roundedValue)."), dismissButton: .default(Text("Awesome")))
-                        })
-                        
+                HStack {
+                    Text("1")
+                        .font(.body)
+                        .bold()
+                    Slider(value: $sliderValue, in:
+                            1.0...100.0)
+                    Text("100")
+                        .font(.body)
+                        .bold()
                     }
+                    .padding()
+                Button(action: {
+                    alertIsVisible = true
+                }) {
+                    Text("hit me".uppercased())
+                        .bold()
+                        .font(.title3)
                 }
+                    .padding(20.0)
+                .background(ZStack {
+                    Color("ButtonColor")
+                    LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+                })
+                    .cornerRadius(21.0)
+                    .foregroundColor(.white)
+                .alert(isPresented: $alertIsVisible,
+                        content: {
+                let roundedValue: Int = Int(sliderValue.rounded())
+                            
+                return Alert(title: Text("Hi there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round."), dismissButton: .default(Text("Awesome")))
+            })
             }
-        }
-        	
-
+        }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
